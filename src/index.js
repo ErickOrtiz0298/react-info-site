e95834e09dd4b3713a0e0a062b16045c08221823
ErickOrtiz0298 <a13003218@alumnos.uady.mx>
2023-06-28 11:39:24 -0600

    Modifique index.html para volver más sencillo y que apunte a index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App/>
+
+/*
+Challenge: Starting from scratch, build and render the 
+HTML for our section project. Check the Google slide for 
+what you're trying to build.
+
+We'll be adding styling to it later.
+
+Hints:
+* The React logo is a file in the project tree, so you can
+  access it by using `src="./react-logo.png" in your image
+  element
+* You can also set the `width` attribute of the image element
+  just like in HTML. In the slide, I have it set to 40px
+ */
+
+const page = (
+  <h1>Testing</h1>
+
 );
+
+ReactDOM.render(page,document.getElementById("root"));
 
