7779ae9d7d6de837d604b76697c8549a7c25932d
ErickOrtiz0298 <a13003218@alumnos.uady.mx>
2023-06-28 16:49:12 -0600

    react info site almost finish

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,10 @@ import ReactDOM  from "react"
 export default function Header(){
     return(
       <header>
-        <nav className="nav">
-          <img src='https://blog.atomikod.com/wp-content/uploads/2020/12/reactjs.jpg' alt='' className='nav-logo'/>
-          <ul className="nav-items">
-                <li>Pricing</li>
-                <li>About</li>
-                <li>Contact</li>
-          </ul>
+        <nav>
+          <img src='https://blog.atomikod.com/wp-content/uploads/2020/12/reactjs.jpg' alt='' className='nav--icon'/>
+          <h3 className="nav--logo_text">ReactFacts</h3>
+          <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
       </header>
     )
